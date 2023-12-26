6adee37331aab3947dc93c75b3131d4e4b8d9517
Lina Kalinka <lina.kalinka@gmail.com>
2023-12-26 21:08:09 +0300

    залить правки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,28 @@
-const cardTemplate = document.querySelector("#card-template").content; //забираем шаблон карточки
-const cardsList = document.querySelector(".places__list"); //забираем список карточек из DOM
+const cardTemplate = document.querySelector('#card-template').content; //забираем шаблон карточки
+const cardsList = document.querySelector('.places__list'); //забираем список карточек из DOM
 
-function addCards(name, link) {
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true); //клонируем элемент карточки из шаблона в переменную
-  const delButton = cardElement.querySelector(".card__delete-button"); //забираем кнопку удаления для карточки
-  cardElement.querySelector(".card__image").src = link; //добавляем ссылку в источник картинки для карточки
-  cardElement.querySelector(".card__image").alt = name; //добавляем alt для картинки
-  cardElement.querySelector(".card__title").textContent = name; //добавляем название карточки
-  delButton.addEventListener("click", () => {
-    delCard(delButton); //по клику выполняем функцию удаления с аргументом-кнопкой
+function createCard(item, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true); //клонируем элемент карточки из шаблона в переменную
+  const delButton = cardElement.querySelector('.card__delete-button'); //забираем кнопку удаления для карточки
+  const cardImg = cardElement.querySelector('.card__image'); //забираем картинку карточки
+  cardImg.src = item.link; //добавляем ссылку в источник картинки для карточки
+  cardImg.alt = item.name; //добавляем alt для картинки
+  cardElement.querySelector('.card__title').textContent = item.name; //добавляем название карточки
+  delButton.addEventListener('click', () => {
+    deleteCard(delButton); //по клику выполняем функцию удаления с аргументом-кнопкой
   });
+  return cardElement; //возвращаем элемент карточки
+}
+
+function addCard(cardElement) {
   cardsList.append(cardElement); //добавляем готовую карточку из переменной в DOM
 }
 
-function delCard(button) {
-  button.closest(".card").remove(); //удаляем ближайший к кнопке-аргументу элемент карточки
+function deleteCard(button) {
+  button.closest('.card').remove(); //удаляем ближайший к кнопке-аргументу элемент карточки
 }
 
 initialCards.forEach((element) => {
-  addCards(element.name, element.link); //выполняем добавление карточки для каждого элемента из массива в cards.js, забирая из элементов имена и ссылки
+  const cardElement = createCard(element, deleteCard); //создаем карточку для каждого элемента из массива в cards.js, забирая из элементов имена и ссылки
+  addCard(cardElement); //добавляем созданные карточки на страницу
 });
