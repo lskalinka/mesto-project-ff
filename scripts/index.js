a98a98273d7b31e1775641e938737ba0bc003fe9
Lina Kalinka <lina.kalinka@gmail.com>
2023-12-26 18:53:22 +0300

    залить проект

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content; //забираем шаблон карточки
+const cardsList = document.querySelector(".places__list"); //забираем список карточек из DOM
 
-// @todo: DOM узлы
+function addCards(name, link) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true); //клонируем элемент карточки из шаблона в переменную
+  const delButton = cardElement.querySelector(".card__delete-button"); //забираем кнопку удаления для карточки
+  cardElement.querySelector(".card__image").src = link; //добавляем ссылку в источник картинки для карточки
+  cardElement.querySelector(".card__image").alt = name; //добавляем alt для картинки
+  cardElement.querySelector(".card__title").textContent = name; //добавляем название карточки
+  delButton.addEventListener("click", () => {
+    delCard(delButton); //по клику выполняем функцию удаления с аргументом-кнопкой
+  });
+  cardsList.append(cardElement); //добавляем готовую карточку из переменной в DOM
+}
 
-// @todo: Функция создания карточки
+function delCard(button) {
+  button.closest(".card").remove(); //удаляем ближайший к кнопке-аргументу элемент карточки
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach((element) => {
+  addCards(element.name, element.link); //выполняем добавление карточки для каждого элемента из массива в cards.js, забирая из элементов имена и ссылки
+});
