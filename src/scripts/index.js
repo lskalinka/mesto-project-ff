b78de565f17edf31c4600090468af2124bbe8596
Lina Kalinka <lina.kalinka@gmail.com>
2024-02-13 23:41:53 +0300

    добавить разметку сообщений об ошибке

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -87,3 +87,14 @@ popups.forEach(function (item) {
 export {
   cardTemplate
 }; //экспортируем глобальную переменную и функцию открытия большой картинки
+
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('form__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('form__input-error_active');
+};
+
+
+
